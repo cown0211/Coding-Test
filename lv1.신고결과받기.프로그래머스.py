89472d61ad819368dce07dce0bcbe0dfae81a7e5
cown0211 <84676665+cown0211@users.noreply.github.com>
2022-05-16 03:52:15 +0900

    Create lv1.신고결과받기.프로그래머스.py

---
+++ lv1.신고결과받기.프로그래머스.py
@@ -0,0 +1,45 @@
+def solution(id_list, report, k):
+    
+    # report에서 중복값 제거, 같은 사람이 같은 사람 신고하면 카운트 안함
+    new_report = list(set(report))
+    
+    # rpt:신고자, rpted:불량이용자
+    rpt,rpted = [],[]
+    for i in range(0, len(new_report)):
+        rpt.append(new_report[i].split(" ")[0])
+        rpted.append(new_report[i].split(" ")[1])
+    
+    # id_list 기준, 신고당한 횟수 카운트
+    cnt = []
+    for i in id_list:
+        cnt.append(rpted.count(i))
+    
+    # id_list 기준, 정지 당한 횟수
+    n = []
+    for i in range(0, len(cnt)):
+        tmp = 0
+        if cnt[i] >= k:
+            tmp += 1
+            n.append(tmp)
+        else:
+            n.append(0)
+            
+    # 정지당한 사람
+    stp = []
+    for i in range(0, len(n)):
+        if n[i] >= 1: stp.append(id_list[i])
+
+    # 정지 당한 사람의 인덱스
+    idx = [i for i, x in enumerate(rpted) if x in stp]
+    
+    # 메일을 받아볼 사람
+    mail = []
+    for i in range(0, len(idx)):
+        mail.append(rpt[idx[i]])
+    
+    # id_list 기준으로 받아볼 메일의 개수
+    answer = []
+    for i in id_list:
+        answer.append(mail.count(i))
+        
+    return answer
